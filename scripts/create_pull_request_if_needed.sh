0f7674be8f801094d133b223d51abd33e95c0046
machida-isedai <62002052+machida-isedai@users.noreply.github.com>
2020-03-10 13:13:58 +0900

    Update create_pull_request_if_needed.sh
    メールアドレス変更

--- scripts/create_pull_request_if_needed.sh
+++ scripts/create_pull_request_if_needed.sh
@@ -2,7 +2,7 @@ export BRANCH=update-data-`date -u "+%Y%m%d"`
 git status -s 2> /dev/null | grep x-ken-all.csv
 if [ "$?" -eq 0 ] ; then
   echo x-ken-all.csv changed
-  git config --global user.email kameda.sbng@gmail.com
+  git config --global user.email shumachi@i-sedai.com
   git config --global user.name 'CircleCI'
   git add docs
   git commit -a -m 'Update data'
