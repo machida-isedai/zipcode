eb3b596415f1f98e5870d7deb048d7a183e7e2c7
machida-isedai <62002052+machida-isedai@users.noreply.github.com>
2020-03-11 09:25:22 +0900

    Update create_pull_request.rb

--- scripts/create_pull_request.rb
+++ scripts/create_pull_request.rb
@@ -2,7 +2,7 @@ require 'octokit'
 
 client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
 client.create_pull_request(
-  'machida-isedai/zipcode_jp',  # 適当に変える
+  'machida-isedai/zipcode',  # 適当に変える
   'master',
   ENV['BRANCH'],
   'Zip data updated',  # Title
